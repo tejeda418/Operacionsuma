5ca62d19e7e26c39e5688e4311e3220682ebf068
tejeda418 <114043816+tejeda418@users.noreply.github.com>
2022-09-21 22:03:14 -0600

    Agregar archivos de proyecto.

---
+++ Clsuma.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operacion
+{
+    internal class clsuma
+    {
+        public clsuma(int n1, int n2)
+        {
+            N1 = n1;
+            N2 = n2;
+        }
+
+        public int N1 { get; }
+        public int N2 { get; }
+
+        public int sumar()
+        {
+            return N1 + N2;
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Operacion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            clsuma osuma = new clsuma(int.Parse(textBox1.Text), int.Parse(textBox2.Text));
+            textBox3.Text = osuma.sumar().ToString();
+        }
+    }
+}
